6d6cbdfb205f5224e99717bee3c27256ca91b0e3
PrakharSharma <89313716+PrakharSharma404@users.noreply.github.com>
2025-05-27 11:06:14 +0530

    Update python.go

--- container-service/preview/python.go
+++ container-service/preview/python.go
@@ -3,14 +3,51 @@ package preview
 import (
 	"fmt"
 	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+// ----------- Prometheus metrics -----------
+
+// Counter to track how many times GetPythonDockerfilePreview is called
+var previewCallCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "python_dockerfile_preview_calls_total",
+		Help: "Total number of Python Dockerfile preview generations",
+	},
+)
+
+// Histogram to measure duration of Dockerfile preview generation
+var previewDuration = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name:    "python_dockerfile_preview_duration_seconds",
+		Help:    "Duration of Python Dockerfile preview generation in seconds",
+		Buckets: prometheus.DefBuckets,
+	},
+)
+
+func init() {
+	// Register metrics with Prometheus default registry
+	prometheus.MustRegister(previewCallCounter)
+	prometheus.MustRegister(previewDuration)
+}
+
+// GetPythonDockerfilePreview generates a Dockerfile string for Python projects
 func GetPythonDockerfilePreview(
 	installCommand string,
 	exposePort string,
 	deployCommand string,
 	environmentVars string,
 ) (string, error) {
+	// Start timer to track how long this function takes
+	timer := prometheus.NewTimer(previewDuration)
+	defer timer.ObserveDuration()
+
+	// Increment counter each time this function is called
+	previewCallCounter.Inc()
+
+	// Convert deployCommand string into a JSON-like array string for CMD
 	cmdParts := strings.Split(deployCommand, " ")
 	cmdString := `[`
 	for i, part := range cmdParts {
@@ -21,6 +58,7 @@ func GetPythonDockerfilePreview(
 	}
 	cmdString += `]`
 
+	// Compose the Dockerfile string using fmt.Sprintf
 	dockerfile := fmt.Sprintf(
 		`FROM python:alpine%v
 WORKDIR /app
