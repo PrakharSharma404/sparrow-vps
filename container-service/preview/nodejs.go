450d7e92538321104ae3aff7df121d4c0a3ec76d
PrakharSharma <89313716+PrakharSharma404@users.noreply.github.com>
2025-05-27 11:04:59 +0530

    Update nodejs.go

--- container-service/preview/nodejs.go
+++ container-service/preview/nodejs.go
@@ -1,7 +1,38 @@
 package preview
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// ----------- Prometheus metrics -----------
+
+// Counter to track how many times GetNodeJSDockerfilePreview is called
+var previewCallCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "nodejs_dockerfile_preview_calls_total",
+		Help: "Total number of NodeJS Dockerfile preview generations",
+	},
+)
+
+// Histogram to measure duration of Dockerfile preview generation
+var previewDuration = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name:    "nodejs_dockerfile_preview_duration_seconds",
+		Help:    "Duration of NodeJS Dockerfile preview generation in seconds",
+		Buckets: prometheus.DefBuckets,
+	},
+)
+
+func init() {
+	// Register metrics with Prometheus default registry
+	prometheus.MustRegister(previewCallCounter)
+	prometheus.MustRegister(previewDuration)
+}
+
+// GetNodeJSDockerfilePreview generates a Dockerfile string for NodeJS projects
 func GetNodeJSDockerfilePreview(
 	nodeVersion string,
 	installCommand string,
@@ -9,6 +40,14 @@ func GetNodeJSDockerfilePreview(
 	outputDirectory string,
 	environmentVars string,
 ) (string, error) {
+	// Start timer to track how long this function takes
+	timer := prometheus.NewTimer(previewDuration)
+	defer timer.ObserveDuration()
+
+	// Increment counter each time this function is called
+	previewCallCounter.Inc()
+
+	// Compose the Dockerfile string using fmt.Sprintf
 	dockerfile := fmt.Sprintf(
 		`FROM node:%v-alpine AS builder%v
 WORKDIR /app
@@ -36,3 +75,4 @@ CMD ["nginx", "-g", "daemon off;"]`,
 
 	return dockerfile, nil
 }
+
