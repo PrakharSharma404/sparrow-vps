b12a92feffdd77e75e7486af43221fdc8e3c3a44
PrakharSharma <89313716+PrakharSharma404@users.noreply.github.com>
2025-05-27 11:07:09 +0530

    Update build.go

--- container-service/routes/build.go
+++ container-service/routes/build.go
@@ -8,12 +8,46 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-// build docker image using given data
+// ----------- Prometheus metrics -----------
+
+// Counter to track total build requests received
+var buildRequestCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "build_requests_total",
+		Help: "Total number of Docker image build requests received",
+	},
+)
+
+// Histogram to measure duration of build request handling
+var buildRequestDuration = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name:    "build_request_duration_seconds",
+		Help:    "Duration of Docker image build request handling in seconds",
+		Buckets: prometheus.DefBuckets,
+	},
+)
+
+func init() {
+	// Register the metrics with Prometheus's default registry
+	prometheus.MustRegister(buildRequestCounter)
+	prometheus.MustRegister(buildRequestDuration)
+}
+
+// HandleBuildRequest builds docker image using given data
 func HandleBuildRequest(c *gin.Context) {
+	// Increment counter for each build request received
+	buildRequestCounter.Inc()
+
+	// Start timer for request duration measurement
+	timer := prometheus.NewTimer(buildRequestDuration)
+	defer timer.ObserveDuration()
+
 	var request types.BuildRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
